7caa8a029b420a22d453042ab551a25a0702aa31
Its-Shashank <gaurshashank014@gmail.com>
2021-04-23 11:46:56 +0530

    added react refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import IMAGE from '../react_logo.png';
 import "./style.css";
+import { Counter } from './components/Counter'
+
 export const App = () => {
-  return <>
-    <h1>React Typescript webpack template - {process.env.NODE_ENV} {process.env.name} </h1>
-    <img src={IMAGE} alt="React Logo" width={600} height={400} />
-  </>;
+  return (
+    <>
+      <h1>React Typescript webpack template - {process.env.NODE_ENV} {process.env.name} </h1>
+      <img src={IMAGE} alt="React Logo" width={600} height={400} />
+      <Counter />
+    </>
+  );
 }
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,11 +1,17 @@
 const webpack = require("webpack");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  devServer: {
+    hot: true,
+    open: true,
+  },
   devtool: "cheap-module-source-map", // read about source maps and devtool more
   plugins: [
     new webpack.DefinePlugin({
       "process.env.name": JSON.stringify("Hanu"),
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 };
