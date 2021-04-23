97b933877ecb1281160fc4e87cce9c88f05fb468
Its-Shashank <gaurshashank014@gmail.com>
2021-04-23 17:15:02 +0530

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module", // allows for the usage of imports
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+    "plugin:jsx-a11y/recommended",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/no-var-requires": "off", // turned off errors from require statements
+    "react/prop-types": "off", // turned off errors from prop-types
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off", // turned off error when there is no return statement
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
